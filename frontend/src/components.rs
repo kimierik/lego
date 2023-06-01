2b8ef2e37461062bceabe9d15ef073ddac73384c
kimierik <kimi.malkamaki@gmail.com>
2023-06-01 10:08:56 +0300

    added functionality now can access files on the backend

--- frontend/src/components.rs
+++ frontend/src/components.rs
@@ -1,4 +1,31 @@
 use leptos::*;
+use serde::{Deserialize,Serialize};
+
+
+
+//data from files
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct RemoteFile{
+    name:String,
+}
+
+
+#[component]
+fn Element(cx:Scope,data:String)->impl IntoView{
+
+    let url="http://localhost:3000/api/redirdoc?name=".to_string() +&data;
+
+    view!{cx,
+        <div>
+            <br/>
+            <a href={url}>
+                    {data}
+            </a>
+            <br/>
+        </div>
+    }
+}
+
 
 
 pub async fn sendreq()-> Result<reqwasm::http::Response, reqwasm::Error>{
@@ -10,7 +37,7 @@ pub async fn sendreq()-> Result<reqwasm::http::Response, reqwasm::Error>{
     thing
 }
 
-pub async fn reqwrap()->String{
+pub async fn reqwrap()->Vec<String>{
     let thing= match sendreq().await {
         Ok(a)=>a,
         Err(e)=>{
@@ -18,7 +45,14 @@ pub async fn reqwrap()->String{
             panic!("{}",e);
         },
     };
-    thing.text().await.unwrap()
+
+    // turn to json
+    thing.json::<Vec<RemoteFile>>()
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|x| x.name)
+        .collect::<Vec<_>>()
 }
 
 #[component]
@@ -27,9 +61,11 @@ pub fn App(cx: Scope)->impl IntoView{
     let once = create_resource(cx, || (), |_| async move { reqwrap().await });
     view! { cx,
         <h1>"server data"</h1>
+            //see if there is data in the data
         {move || match once.read(cx) {
             None => view! { cx, <p>"Loading..."</p> }.into_view(cx),
-            Some(data) => view! { cx,<p>{data}</p>  }.into_view(cx)
+            Some(data) => data.into_iter().map(|item|{ view! {cx, <Element data=item/>} }).collect_view(cx)
+                          //since data is a vec we will iterate it and make views out of them
         }}
     }
 
